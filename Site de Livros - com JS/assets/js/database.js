ed602ae3cb0142b97566c46e8946c87e001802af
Fabio Junior <91484736+Fabio-jr-SM@users.noreply.github.com>
2022-08-14 14:57:19 -0300

    Update Site de Livros

---
+++ Site de Livros - com JS/assets/js/database.js
@@ -0,0 +1,20 @@
+const baseLivros = [
+    {
+        'name': 'O que Aconteceu com Annie',
+        'author': 'C.J. Tudor',
+        'path': './assets/img_livros/O que aconteceu com Annie.jpeg',
+        'tipo': 'Mistério',
+    },
+    {
+        'name': 'A garota no Lago',
+        'author': 'Desconhecido',
+        'path': './assets/img_livros/A garota no Lago.jpg',
+        'tipo': 'Mistério',
+    },
+    {
+        'name': 'Homem de Giz',
+        'author': 'C.J. Tudor',
+        'path': './assets/img_livros/Homem de Giz.jpg',
+        'tipo': 'Mistério',
+    }
+]
---
+++ Site de Livros - com JS/assets/js/script.js
@@ -0,0 +1,11 @@
+
+
+function buttonNext(){
+    
+    titulo = document.getElementById('titulo') = baseLivros[1];
+    const nomeAlbum = document.getElementById('nomeAlbum');
+    
+   // fotoAlbum.src = foto
+    nomeMusica.innerText = musica
+    nomeAlbum.innerText = nome
+}
